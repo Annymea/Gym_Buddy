d616ebadb3d2d0b7d9a9a2472895962e5ac0ee5b
Annymea <anna.grahl@gmx.de>
2024-08-10 21:43:06 +0200

    add two buttons to main activity

--- app/src/main/java/com/example/GymBuddy/MainActivity.kt
+++ app/src/main/java/com/example/GymBuddy/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.GymBuddy.ui.theme.Gym_BuddyTheme
 
@@ -20,10 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Gym_BuddyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    FirstButtons(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +31,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FirstButtons(modifier: Modifier) {
+    Column(modifier = modifier) {
+        Button(
+            onClick = { /*TODO*/ }
+        ) {
+            Text(text = stringResource(R.string.create_plan))
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = stringResource(R.string.start_training))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Gym_BuddyTheme {
-        Greeting("Android")
+        FirstButtons(Modifier)
     }
 }
