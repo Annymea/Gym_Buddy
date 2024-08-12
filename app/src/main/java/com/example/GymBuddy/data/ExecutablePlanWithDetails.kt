185467ace352fad5123493bb295a75fb06b2b13b
Annymea <anna.grahl@gmx.de>
2024-08-12 21:30:32 +0200

    add smaller adaptions and getExecutablePlanWithDetailsByPlanId query

---
+++ app/src/main/java/com/example/GymBuddy/data/ExecutablePlanWithDetails.kt
@@ -0,0 +1,11 @@
+package com.example.gymbuddy.data
+
+data class ExecutablePlanWithDetails(
+    val executablePlanId: Int,
+    val planId: Int,
+    val planName: String,
+    val exerciseId: Int,
+    val exerciseName: String,
+    val sets: Int,
+    val order: Int
+)
