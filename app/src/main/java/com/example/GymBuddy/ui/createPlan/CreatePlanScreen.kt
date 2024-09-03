e17e07d945276754cc131fca2f6c67862df268f1
Annymea <anna.grahl@gmx.de>
2024-09-03 17:56:25 +0200

    delete circular progress bar due to errors

--- app/src/main/java/com/example/GymBuddy/ui/createPlan/CreatePlanScreen.kt
+++ app/src/main/java/com/example/GymBuddy/ui/createPlan/CreatePlanScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -16,7 +15,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import org.koin.androidx.compose.koinViewModel
@@ -50,11 +48,12 @@ fun CreatePlan(
                 }
 
                 is SavingPlanState.Saving -> {
-                    CircularProgressIndicator(
-                        modifier = Modifier.align(
-                            Alignment.CenterVertically
-                        )
-                    )
+                    Text(text = "Saving...")
+                    // CircularProgressIndicator(
+                    //    modifier = Modifier.align(
+                    //        Alignment.CenterVertically
+                    //    )
+                    // )
                 }
 
                 is SavingPlanState.Saved -> {
