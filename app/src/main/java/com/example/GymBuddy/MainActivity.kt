e70e983c4041b67f72d750712ff2c5564bc3b132
Annymea <anna.grahl@gmx.de>
2024-09-08 18:43:43 +0200

    implement bottom navigation bar

--- app/src/main/java/com/example/GymBuddy/MainActivity.kt
+++ app/src/main/java/com/example/GymBuddy/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import com.example.GymBuddy.ui.navigation.AppNavigation
 import com.example.GymBuddy.ui.theme.Gym_BuddyTheme
 
 class MainActivity : ComponentActivity() {
