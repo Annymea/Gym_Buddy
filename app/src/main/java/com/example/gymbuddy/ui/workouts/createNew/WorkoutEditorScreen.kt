3c4da4aaa6a25e5ebcd8e32ecd952fb087523bd6
Annymea <anna.grahl@gmx.de>
2024-09-27 23:53:36 +0200

    add WorkoutEditorScreen

---
+++ app/src/main/java/com/example/gymbuddy/ui/workouts/createNew/WorkoutEditorScreen.kt
@@ -0,0 +1,18 @@
+package com.example.gymbuddy.ui.workouts.createNew
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.gymbuddy.ui.workouts.common.ScreenTitle
+
+@Composable
+fun WorkoutEditorScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.fillMaxSize()) {
+        ScreenTitle(text = "Workout Editor")
+
+
+    }
+}
