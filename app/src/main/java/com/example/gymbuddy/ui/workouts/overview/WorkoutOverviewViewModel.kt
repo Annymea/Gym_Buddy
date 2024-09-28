11bf4fb2462a13d9af0d37e8b8e78eb6785da6df
Annymea <anna.grahl@gmx.de>
2024-09-28 21:13:54 +0200

    add catch to WorkoutOverviewViewModel init

--- app/src/main/java/com/example/gymbuddy/ui/workouts/overview/WorkoutOverviewViewModel.kt
+++ app/src/main/java/com/example/gymbuddy/ui/workouts/overview/WorkoutOverviewViewModel.kt
@@ -8,10 +8,12 @@ import com.example.gymbuddy.data.WorkoutRepository
 import com.example.gymbuddy.data.localdatabase.Plan
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 
 sealed class WorkoutOverviewUiState {
     data object NoWorkouts : WorkoutOverviewUiState()
+
     data object Workouts : WorkoutOverviewUiState()
 }
 
@@ -20,8 +22,10 @@ interface WorkoutOverviewViewModelContract {
     val uiState: StateFlow<WorkoutOverviewUiState>
 }
 
-class WorkoutOverviewViewModel(workoutRepository: WorkoutRepository) :
-    WorkoutOverviewViewModelContract, ViewModel() {
+class WorkoutOverviewViewModel(
+    workoutRepository: WorkoutRepository,
+) : ViewModel(),
+    WorkoutOverviewViewModelContract {
     private var _workouts: SnapshotStateList<Plan> = mutableStateListOf()
     override val workouts: List<Plan>
         get() = _workouts
@@ -32,16 +36,21 @@ class WorkoutOverviewViewModel(workoutRepository: WorkoutRepository) :
 
     init {
         viewModelScope.launch {
-            workoutRepository.getAllPlanNames().collect { workouts ->
-                _workouts.clear()
-                _workouts.addAll(workouts)
-
-                if (_workouts.isNotEmpty()) {
-                    _uiState.value = WorkoutOverviewUiState.Workouts
-                } else {
+            workoutRepository
+                .getAllPlanNames()
+                .catch {
+                    _workouts.clear()
                     _uiState.value = WorkoutOverviewUiState.NoWorkouts
+                }.collect { workouts ->
+                    _workouts.clear()
+                    _workouts.addAll(workouts)
+
+                    if (_workouts.isNotEmpty()) {
+                        _uiState.value = WorkoutOverviewUiState.Workouts
+                    } else {
+                        _uiState.value = WorkoutOverviewUiState.NoWorkouts
+                    }
                 }
-            }
         }
     }
 }
