ecd501104b889ffb768903271a61d4eb7ad3acb9
Annymea <anna.grahl@gmx.de>
2024-12-24 00:07:30 +0100

    feat: reordering works and does not look wrong
    Signed-off-by: Annymea <anna.grahl@gmx.de>

--- app/src/main/java/com/example/gymbuddy/ui/common/draggableList/DraggableList.kt
+++ app/src/main/java/com/example/gymbuddy/ui/common/draggableList/DraggableList.kt
@@ -17,10 +17,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
@@ -33,8 +35,6 @@ fun <T> DraggableLazyColumn(
     itemContent: @Composable (item: T) -> Unit,
 
     ) {
-    //TODO: Der Ramen des Schattens sieht noch etwas weird auch. Das muss ich noch ausbessern
-
     val mutableItems = remember { items.toMutableStateList() }
     var draggingIndex by remember { mutableStateOf<Int?>(null) }
     var offsetY by remember { mutableFloatStateOf(0f) }
@@ -56,20 +56,31 @@ fun <T> DraggableLazyColumn(
                 if (isBeingDragged) 8.dp else 0.dp,
                 label = "showShadow"
             )
+
+            //ToDO: evtl hier noch das Item anders aussehen lassen. Passt aber erstmal
             val alpha by animateFloatAsState(
                 if (isBeingDragged) 0.9f else 1f,
                 label = "makeItemLessVisible"
             )
 
+            val hapticFeedback = LocalHapticFeedback.current
+
+            var itemHeightPx by remember { mutableFloatStateOf(0f) }
+
             Box(
                 modifier = Modifier
+                    .onGloballyPositioned { coordinates ->
+                        itemHeightPx = coordinates.size.height.toFloat()
+                    }
                     .offset { IntOffset(0, animatedOffset.roundToPx()) }
                     .zIndex(if (isBeingDragged) 1f else 0f)
-                    .shadow(shadowElevation, shape = RoundedCornerShape(8.dp))
-                    .alpha(alpha)
+                    .shadow(elevation = shadowElevation, shape = RoundedCornerShape(8.dp))
                     .pointerInput(Unit) {
                         detectDragGesturesAfterLongPress(
-                            onDragStart = { draggingIndex = index },
+                            onDragStart = {
+                                draggingIndex = index
+                                hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+                            },
                             onDrag = { _, dragAmount ->
                                 offsetY += dragAmount.y
                                 draggingIndex?.let { dragging ->
@@ -77,8 +88,7 @@ fun <T> DraggableLazyColumn(
                                         draggingIndex = dragging,
                                         dragOffset = offsetY,
                                         listSize = items.size,
-                                        // i would like to use actual height here
-                                        itemHeightPx = with(density) { 56.dp.toPx() }
+                                        itemHeightPx = itemHeightPx
                                     )
                                     if (targetIndex != dragging) {
                                         mutableItems.swap(dragging, targetIndex)
